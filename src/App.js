e50dbe2ba1dfef787ec7155c952249a13229c940
Elizabethg <Elizabethg@gnrgy.com>
2024-04-04 15:41:28 +0530

    [Msal-Azure] Registered app with Azure Id and got client id and other parameters

--- src/App.js
+++ src/App.js
@@ -10,8 +10,9 @@ import { PublicClientApplication } from "@azure/msal-browser";
 
 const pca = new PublicClientApplication({
   auth: {
-    clientId: "",
-    authority: "",
+    clientId: "482f7284-f662-4f46-8094-ff53becf6cad", // get it from msal-react-demo (overiew/ application id)
+    authority:
+      "https://login.microsoftonline.com/b35b51f3-994b-4ff8-85d5-ef6995f6121d", // (overiew / (Endpoints /OAuth 2.0 authorization endpoint (v2)))
     redirectUri: "/",
   },
 });
