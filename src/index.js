3935f784dd5c5132bb65dc3bb5184d4357ac04a9
Elizabethg <Elizabethg@gnrgy.com>
2024-04-04 15:45:50 +0530

    [Msal-Azure] Instantiated the public client application from msal broswer at the right component

--- src/index.js
+++ src/index.js
@@ -8,6 +8,17 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from './App';
 
+import { PublicClientApplication } from "@azure/msal-browser";
+
+const pca = new PublicClientApplication({
+  auth: {
+    clientId: "482f7284-f662-4f46-8094-ff53becf6cad", // get it from msal-react-demo (overiew/ application id)
+    authority:
+      "https://login.microsoftonline.com/b35b51f3-994b-4ff8-85d5-ef6995f6121d", // (overiew / (Endpoints /OAuth 2.0 authorization endpoint (v2)))
+    redirectUri: "/",
+  },
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
