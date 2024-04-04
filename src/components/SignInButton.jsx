01136ed68cfb1c0ae5f1c58ea23b6e20b3aa3096
Elizabethg <Elizabethg@gnrgy.com>
2024-04-04 16:08:54 +0530

    [Msal-Azure] Using useMsal hook provided by the msal-react
    1. Using useMsal hook provided by the msal-react
    2. Getting access to the instance which we created earlier

--- src/components/SignInButton.jsx
+++ src/components/SignInButton.jsx
@@ -1,6 +1,11 @@
 import Button from '@mui/material/Button';
 
+import { useMsal } from '@azure/msal-react';
+
 export const SignInButton = () => {
+
+    const { instance } = useMsal();
+    
     return (
         <Button color="inherit">Sign in</Button>
     )
